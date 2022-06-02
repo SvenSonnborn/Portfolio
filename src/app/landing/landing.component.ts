5ebd05b972e48bcd08d774ffa451cebb75b091c5
Sven Sonnborn <sonnborn.sven@web.de>
2022-06-02 14:31:27 +0200

    skill+landing area ngfor, shorter and cleaner code

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,10 +1,6 @@
-import {
-  Component,
-  OnInit,
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InViewPortService } from '../in-view-port.service';
 import { ViewportScroller } from '@angular/common';
-
 
 @Component({
   selector: 'app-landing',
@@ -15,59 +11,44 @@ export class LandingComponent implements OnInit {
   /**
    * We need the text as a Character Array for *ngFor to work. ['H', 'i', ...]
    */
-  introText1 = [
-    ...'Hi,',
-  ];
-  introText1_1 = [
-    ...'My Name is ',
-  ];
-  introText2 = [
-    ...'Sven Sonnborn.',
-  ];
-  introText3 = [
-    ...'I am a ',
-  ];
-  introText4 = [
-    ...'Front-End ',
-  ];
-  introText5 = [
-    ...'Developer.',
-  ];
- 
 
-  constructor(public inviewport: InViewPortService, private viewportScroller: ViewportScroller) {}
-  
-  ngOnInit(): void {
+  introText = [
+    'Hi,',
+    'My Name is',
+    'Sven Sonnborn.',
+    'I am a',
+    'Front-End',
+    'Developer.'
+  ];
+  textlength = [
+    0,
+    30,
+    130,
+    270,
+    330,
+    420
+  ];
 
+  getArrayFromStringArray(string : string){
+      return Array.from(string);
   }
 
-  public onClick(elementId: string): void { 
+  constructor(
+    public inviewport: InViewPortService,
+    private viewportScroller: ViewportScroller
+  ) {}
+
+  ngOnInit(): void {}
+
+  public onClick(elementId: string): void {
     this.viewportScroller.scrollToAnchor(elementId);
-}
+  }
 
   handleViewChange(event: any) {
     console.log(event);
   }
 
-  getAnimationDelay1(index: number) {
-    return `${10 * index}ms`;
+  getAnimationDelay(index: number) {
+    return `${(100 * index) + (3)}ms`;
   }
-
-  getAnimationDelay1_1(index: number) {
-    return `${(10 * index)+ 35}ms`;
-  }
-
-  getAnimationDelay2(index: number) {
-    return `${(10 * index)+ 75}ms`;
-  }
-  getAnimationDelay3(index: number) {
-    return `${(10 * index)+ 200}ms`;
-  }
-  getAnimationDelay4(index: number) {
-    return `${(10 * index)+ 270}ms`;
-  }
-  getAnimationDelay5(index: number) {
-    return `${(10 * index)+ 370}ms`;
-  }
-
 }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -2,25 +2,46 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
+  Input,
   OnInit,
   ViewChild,
+  Directive,
 } from '@angular/core';
 import { InViewPortService } from '../in-view-port.service';
-
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
+
+
+
 export class SkillsComponent implements OnInit {
+  
 
   constructor(public inviewport: InViewPortService) { }
 
   ngOnInit(): void {
   }
+
+  skillsJSON: any[] = [
+    {"name" : 'Javascript',       "Icon" : 'java script',   "class" : 'fly-in-left'},
+    {"name" : 'Angular',          "Icon" : 'angular2',      "class" : 'grow'},
+    {"name" : 'HTML / CSS',       "Icon" : 'html',          "class" : 'fly-in-right'},
+    {"name" : 'SCRUM',            "Icon" : 'scrum',         "class" : 'fly-in-left'},
+    {"name" : 'GIT',              "Icon" : 'git',           "class" : 'grow'},
+    {"name" : 'Design Thinking',  "Icon" : 'pen',           "class" : 'fly-in-right'},
+    {"name" : 'Rest API',         "Icon" : 'api',           "class" : 'fly-in-left'},
+    {"name" : 'Test automation',  "Icon" : 'test',          "class" : 'grow'},
+    {"name" : 'Databases',        "Icon" : 'database',      "class" : 'fly-in-right'}
+  ];
   
   handleViewChange(event: any) {
     console.log(event);
+  }
+  
+  divisableby2(){
+    return this.skillsJSON.length % 2 === 0
   }
 }
